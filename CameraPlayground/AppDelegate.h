6aac4e3797ce464a8e4167ede55f76ced0520050
Matt Fenwick <matt.fenwick@hudl.com>
2015-02-06 14:48:31 -0600

    Initial Commit

---
+++ CameraPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraPlayground
+//
+//  Created by Matthew Fenwick on 2/6/15.
+//  Copyright (c) 2015 Matthew Fenwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraPlayground
+//
+//  Created by Matthew Fenwick on 2/6/15.
+//  Copyright (c) 2015 Matthew Fenwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
