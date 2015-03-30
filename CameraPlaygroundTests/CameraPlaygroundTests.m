6d5c00fc7d6db4db3e1f0b15e08b4c76a9fcf860
Matt Fenwick <matt.fenwick@hudl.com>
2015-03-29 19:05:18 -0500

    unit tests for formats

--- CameraPlaygroundTests/CameraPlaygroundTests.m
+++ CameraPlaygroundTests/CameraPlaygroundTests.m
@@ -8,12 +8,37 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "CameraController.h"
+
 
 @interface CameraPlaygroundTests : XCTestCase
 
+@property (nonatomic) NSInteger fileCount;
+
 @end
 
+
 @implementation CameraPlaygroundTests
+
+- (NSURL *)getFileURL
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    if ([paths count] == 0)
+    {
+        return nil;
+    }
+    NSString *documentsPath = [paths objectAtIndex:0];
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:[NSString stringWithFormat:@"test_%ld.mov", (long)self.fileCount]];
+    self.fileCount++;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if ([fileManager fileExistsAtPath:filePath])
+    {
+        [fileManager removeItemAtPath:filePath error:nil];
+    }
+    return [NSURL fileURLWithPath:filePath];
+}
+
+#pragma mark - scaffolding
 
 - (void)setUp {
     [super setUp];
@@ -25,9 +50,61 @@
     [super tearDown];
 }
 
+#pragma mark - tests
+
 - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
+}
+
+- (void)testFormats
+{
+    CameraController *controller = [[CameraController alloc] initWithUsingCustomPipeline:YES];
+    [controller initializeAVCaptureSessionWithCameraPosition:AVCaptureDevicePositionBack];
+    [controller setVideoAVCaptureOrientation:AVCaptureVideoOrientationPortrait];
+    __block NSInteger count = 1;
+    __block BOOL exitLoop = NO;
+    for (AVCaptureDeviceFormat *format in [controller.camera formats])
+    {
+        if ([controller.camera lockForConfiguration:nil])
+        {
+            controller.camera.activeFormat = format;
+            [controller.camera unlockForConfiguration];
+            NSURL *url = [self getFileURL];
+            CameraControllerError startErrorCode = [controller startRecordingWithFileURL:url];
+            if (startErrorCode == CameraControllerErrorNone)
+            {
+                dispatch_after(0.5, dispatch_get_main_queue(), ^() {
+                    CameraControllerError stopErrorCode = [controller stopRecording];
+                    if (stopErrorCode == CameraControllerErrorNone)
+                    {
+                        count++;
+                        XCTAssert(YES, @"successfully started and stopped recording for format");
+                    }
+                    else
+                    {
+                        XCTAssert(NO, @"failed to stop recording for format");
+                        exitLoop = YES;
+                    }
+                });
+            }
+            else
+            {
+                NSString *errorString = [NSString stringWithFormat:@"failed to start recording for format -- %@, %ld", [format description], startErrorCode];
+                NSLog(@"%@", errorString);
+                XCTAssert(NO, @"oops");
+                exitLoop = YES;
+            }
+        }
+        else
+        {
+            NSLog(@"unable to lock for configuration");
+        }
+        if (exitLoop)
+        {
+            break;
+        }
+    }
 }
 
 - (void)testPerformanceExample {
